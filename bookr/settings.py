f4126a4209a56faaa1932833713a5daa535a1492
Baochuan Lu <blu@sbuniv.edu>
2025-01-16 13:11:42 -0600

    add console logging

--- bookr/settings.py
+++ bookr/settings.py
@@ -123,3 +123,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',  # Adjust the level as needed (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        },
+    },
+}
+
+# settings.py
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger('django')
 
 def index(request):
+    logger.info("Hello from my_view!")
     return HttpResponse("Hello, world!")
+
+# views.py (or any other module)
