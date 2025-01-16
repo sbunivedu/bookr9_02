4bd73093f02cc58d79b2d6cf98b06daf18e4f9b5
Baochuan Lu <blu@sbuniv.edu>
2025-01-16 15:14:13 -0600

    exercise 1.06

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 import logging
 
 logger = logging.getLogger('django')
 
 def index(request):
-    logger.info("Hello from my_view!")
-    name = request.GET.get("name") or "world"
-    return HttpResponse("Hello, {}!".format(name))
+    logger.info("Hello from reviews.view!")
+    return render(request, "base.html")
 
 # views.py (or any other module)
