d9abaad02ed6a619b8c95d4f7ffceafd76a97112
Baochuan Lu <blu@sbuniv.edu>
2025-01-21 10:50:14 -0600

    activity 1.01 1.02

--- reviews/views.py
+++ reviews/views.py
@@ -8,4 +8,8 @@ def index(request):
     name = "world"
     return render(request, "base.html", {"name": name})
 
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html",
+    {"search_text": search_text})
 # views.py (or any other module)
