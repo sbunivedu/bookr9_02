3569b88694a49cc45e10cc956dfe62372b1ae7c4
Baochuan Lu <blu@sbuniv.edu>
2025-01-23 14:39:17 -0600

    activity 3.01

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from .utils import average_rating
+
 def index(request):
     return render(request, "reviews/base.html")
 def book_search(request):
@@ -28,3 +29,21 @@ def book_list(request):
         'book_list': book_list
     }
     return render(request, 'reviews/book_list.html', context)
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
+        }
+    else:
+        context = {
+            "book": book,
+            "book_rating": None,
+            "reviews": None
+        }
+    return render(request, "reviews/book_detail.html", context)
