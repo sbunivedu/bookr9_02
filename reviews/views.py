feb9ac517e380d0b2641879869e2bfde3ae729ea
Baochuan Lu <blu@sbuniv.edu>
2025-01-23 11:45:18 -0600

    exercise 3.04

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Book
 from .utils import average_rating
 def index(request):
-    return render(request, "base.html")
+    return render(request, "reviews/base.html")
 def book_search(request):
     search_text = request.GET.get("search", "")
     return render(request,
