33729f134641aaef264327d3b8b0bf99b0fbfbfb
Baochuan Lu <blu@sbuniv.edu>
2025-01-23 11:22:17 -0600

    exercise 3.03

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.welcome_view, name='welcome_view'),
+    path('books/', views.book_list, name='book_list'),
 ]
