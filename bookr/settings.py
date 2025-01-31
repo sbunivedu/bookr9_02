3a058ab9d1bee11db03631cd8448af335e0515ea
Baochuan Lu <blu@sbuniv.edu>
2025-01-31 15:54:50 -0600

    before activity 4.02

--- bookr/settings.py
+++ bookr/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'reviews.adminconfig.ReviewsAdminConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- bookr/urls.py
+++ bookr/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from reviews.admin import admin_site
+from reviews.admin import admin
 from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookradmin/', admin_site.urls),
+    path('bookradmin/', admin.site.urls),
 ]
