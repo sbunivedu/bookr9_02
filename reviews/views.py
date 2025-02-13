475dc5bface6b25e06d452bf6ff3d27e4b4facd0
Baochuan Lu <blu@sbuniv.edu>
2025-02-13 13:14:44 -0600

    activity 6.01 add search in contributors

--- reviews/views.py
+++ reviews/views.py
@@ -16,9 +16,26 @@ def book_search(request):
     print(f"form.is_valid()=>{form.is_valid()}")
     print(f"form.cleaned_data['search']=>{form.cleaned_data['search']}")
 
-    if form.is_valid() and form.cleaned_data["search"]:
-        search = form.cleaned_data["search"]
+    search = form.cleaned_data["search"]
+    search_in = form.cleaned_data.get("search_in") or "title" # default to search in/by title
+    if search_in == "title":
         books = Book.objects.filter(title__icontains=search)
+    else:
+        fname_contributors = Contributor.objects.filter(
+            first_names__icontains=search
+        )
+
+        for contributor in fname_contributors:
+            for book in contributor.book_set.all():
+                books.add(book)
+
+        lname_contributors = Contributor.objects.filter(
+            last_names__icontains=search
+        )
+
+        for contributor in lname_contributors:
+            for book in contributor.book_set.all():
+                books.add(book)
 
     return render(
         request,
