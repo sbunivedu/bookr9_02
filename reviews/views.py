b079ea4ac37c32e76b29d8222f0a723b2e361643
Baochuan Lu <blu@sbuniv.edu>
2025-02-13 13:01:38 -0600

    use django form to search books by title

--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book
+from .forms import SearchForm
+from .models import Book, Contributor
 from .utils import average_rating
 
 def index(request):
     return render(request, "base.html")
 
 def book_search(request):
+    print(request.GET) # for debugging
+
     search_text = request.GET.get("search", "")
-    return render(request,
-    "reviews/search-results.html",
-        {"search_text": search_text})
+    form = SearchForm(request.GET)
+    books = set()
+
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        books = Book.objects.filter(title__icontains=search)
+
+    return render(
+        request,
+        "reviews/search-results.html",
+        {"form": form, "search_text": search_text, "books": books},
+    )
 
 def book_list(request):
     books = Book.objects.all()
