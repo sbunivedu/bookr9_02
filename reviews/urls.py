5ce407c5acdcbe27dae1eaba1f0d9177348aee10
Baochuan Lu <blu@sbuniv.edu>
2025-02-18 17:08:01 -0600

    separate review_create view action logic from review_edit

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("books/<int:pk>/", views.book_detail, name="book_detail"),
     path("publishers/<int:pk>/", views.publisher_edit, name="publisher_edit"),
     path("publishers/new/", views.publisher_edit, name="publisher_create"),
-    path("books/<int:book_pk>/reviews/new/", views.review_edit, name="review_create"),
+    path("books/<int:book_pk>/reviews/new/", views.review_create, name="review_create"),
     path(
         "books/<int:book_pk>/reviews/<int:review_pk>/",
         views.review_edit,
--- reviews/views.py
+++ reviews/views.py
@@ -113,27 +113,42 @@ def publisher_edit(request, pk=None):
         },
     )
 
-
-def review_edit(request, book_pk, review_pk=None):
+def review_create(request, book_pk):
     book = get_object_or_404(Book, pk=book_pk)
-
-    if review_pk is not None:
-        review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(False)
+            review.book = book
+            messages.success(request, 'Review for "{}" created.'.format(book))
+            review.save()
+            return redirect("book_detail", book.pk)
     else:
-        review = None
+        form = ReviewForm()
+
+    return render(
+        request,
+        "reviews/instance-form.html",
+        {
+            "form": form,
+            "instance": None,
+            "model_type": "Review",
+            "related_instance": book,
+            "related_model_type": "Book",
+        },
+    )
+
+def review_edit(request, book_pk, review_pk):
+    book = get_object_or_404(Book, pk=book_pk)
+    review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
 
     if request.method == "POST":
         form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
             updated_review = form.save(False)
             updated_review.book = book
-
-            if review is None:
-                messages.success(request, 'Review for "{}" created.'.format(book))
-            else:
-                updated_review.date_edited = timezone.now()
-                messages.success(request, 'Review for "{}" updated.'.format(book))
-
+            updated_review.date_edited = timezone.now()
+            messages.success(request, 'Review for "{}" updated.'.format(book))
             updated_review.save()
             return redirect("book_detail", book.pk)
     else:
