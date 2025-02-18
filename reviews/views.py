67917bb532d9328ed82ce26352835f2f29ba40cc
Baochuan Lu <blu@sbuniv.edu>
2025-02-18 11:56:18 -0600

    activity 7.02 – reviewing the creation UI

--- reviews/views.py
+++ reviews/views.py
@@ -1,48 +1,49 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import SearchForm, PublisherForm
-from .models import Book, Contributor, Publisher
+from django.utils import timezone
+
+from .forms import SearchForm, PublisherForm, ReviewForm
+from .models import Book, Contributor, Publisher, Review
 from .utils import average_rating
+
 
 def index(request):
     return render(request, "base.html")
 
-def book_search(request):
-    print(request.GET) # for debugging
 
+def book_search(request):
     search_text = request.GET.get("search", "")
     form = SearchForm(request.GET)
     books = set()
 
-    print(f"form.is_valid()=>{form.is_valid()}")
-    print(f"form.cleaned_data['search']=>{form.cleaned_data['search']}")
+    if form.is_valid() and form.cleaned_data["search"]:
+        search = form.cleaned_data["search"]
+        search_in = form.cleaned_data.get("search_in") or "title"
+        if search_in == "title":
+            books = Book.objects.filter(title__icontains=search)
+        else:
+            fname_contributors = Contributor.objects.filter(
+                first_names__icontains=search
+            )
 
-    search = form.cleaned_data["search"]
-    search_in = form.cleaned_data.get("search_in") or "title" # default to search in/by title
-    if search_in == "title":
-        books = Book.objects.filter(title__icontains=search)
-    else:
-        fname_contributors = Contributor.objects.filter(
-            first_names__icontains=search
-        )
+            for contributor in fname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
 
-        for contributor in fname_contributors:
-            for book in contributor.book_set.all():
-                books.add(book)
+            lname_contributors = Contributor.objects.filter(
+                last_names__icontains=search
+            )
 
-        lname_contributors = Contributor.objects.filter(
-            last_names__icontains=search
-        )
-
-        for contributor in lname_contributors:
-            for book in contributor.book_set.all():
-                books.add(book)
+            for contributor in lname_contributors:
+                for book in contributor.book_set.all():
+                    books.add(book)
 
     return render(
         request,
         "reviews/search-results.html",
         {"form": form, "search_text": search_text, "books": books},
     )
+
 
 def book_list(request):
     books = Book.objects.all()
@@ -55,32 +56,28 @@ def book_list(request):
         else:
             book_rating = None
             number_of_reviews = 0
-        book_list.append({'book': book,
-                          'book_rating': book_rating,
-                          'number_of_reviews': number_of_reviews})
+        book_list.append(
+            {
+                "book": book,
+                "book_rating": book_rating,
+                "number_of_reviews": number_of_reviews,
+            }
+        )
 
-    context = {
-        'book_list': book_list
-    }
-    return render(request, 'reviews/book_list.html', context)
+    context = {"book_list": book_list}
+    return render(request, "reviews/book_list.html", context)
+
 
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     reviews = book.review_set.all()
     if reviews:
         book_rating = average_rating([review.rating for review in reviews])
-        context = {
-            "book": book,
-            "book_rating": book_rating,
-            "reviews": reviews
-        }
+        context = {"book": book, "book_rating": book_rating, "reviews": reviews}
     else:
-        context = {
-            "book": book,
-            "book_rating": None,
-            "reviews": None
-        }
+        context = {"book": book, "book_rating": None, "reviews": None}
     return render(request, "reviews/book_detail.html", context)
+
 
 def publisher_edit(request, pk=None):
     if pk is not None:
@@ -105,6 +102,51 @@ def publisher_edit(request, pk=None):
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(request, "reviews/instance-form.html",
-    {"form": form, "instance": publisher,
-            "model_type": "Publisher"})
+    return render(
+        request,
+        "reviews/instance-form.html",
+        {
+            "method": request.method,
+            "form": form,
+            "model_type": "Publisher",
+            "instance": publisher,
+        },
+    )
+
+
+def review_edit(request, book_pk, review_pk=None):
+    book = get_object_or_404(Book, pk=book_pk)
+
+    if review_pk is not None:
+        review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+    else:
+        review = None
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            updated_review = form.save(False)
+            updated_review.book = book
+
+            if review is None:
+                messages.success(request, 'Review for "{}" created.'.format(book))
+            else:
+                updated_review.date_edited = timezone.now()
+                messages.success(request, 'Review for "{}" updated.'.format(book))
+
+            updated_review.save()
+            return redirect("book_detail", book.pk)
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(
+        request,
+        "reviews/instance-form.html",
+        {
+            "form": form,
+            "instance": review,
+            "model_type": "Review",
+            "related_instance": book,
+            "related_model_type": "Book",
+        },
+    )
