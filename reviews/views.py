3de365b4a482eedbc552451be8de3a8799ff3f1e
Baochuan Lu <blu@sbuniv.edu>
2025-03-04 12:43:28 -0600

    exercise 9.06 – storing recently viewed books in sessions

--- reviews/views.py
+++ reviews/views.py
@@ -80,6 +80,17 @@ def book_detail(request, pk):
         context = {"book": book, "book_rating": book_rating, "reviews": reviews}
     else:
         context = {"book": book, "book_rating": None, "reviews": None}
+
+    if request.user.is_authenticated:
+        max_viewed_books_length = 10
+        viewed_books = request.session.get("viewed_books", [])
+        viewed_book = [book.id, book.title]
+        if viewed_book in viewed_books:
+            viewed_books.pop(viewed_books.index(viewed_book))
+        viewed_books.insert(0, viewed_book)
+        viewed_books = viewed_books[:max_viewed_books_length]
+        request.session["viewed_books"] = viewed_books
+
     return render(request, "reviews/book_detail.html", context)
 
 def is_staff_user(user):
